0d685352242147b9817c16efa4746e89362bbc4a
ssttv <sergey.sergeevich.titov@protonmail.com>
2018-04-15 22:51:22 +0300

    Update friends_of_friends.py

--- friends_of_friends.py
+++ friends_of_friends.py
@@ -1,14 +1,14 @@
-m=[]
+f=[]
 s=[]
 for i in range(int(input())):
     l=input().split()
     k=len(l)
     s.append(l[0])
     for i in range(2,k):
-        m.append(l[i])
-m=set(m)
+        f.append(l[i])
+f=set(f)
 p=len(s)
 for j in range(p):
-    if s[j] in m:
-        m.pop()
-print(len(m))
+    if s[j] in f:
+        f.pop()
+print(len(f))
